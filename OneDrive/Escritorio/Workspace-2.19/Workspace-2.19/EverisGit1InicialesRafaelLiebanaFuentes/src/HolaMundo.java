adcdc2e24395e34655e6fafebc399c8620cb335c
rafliefue <rafliefue@gmail.com>
2021-11-08 11:52:37 +0100

    Cambio en el texto

--- OneDrive/Escritorio/Workspace-2.19/Workspace-2.19/EverisGit1InicialesRafaelLiebanaFuentes/src/HolaMundo.java
+++ OneDrive/Escritorio/Workspace-2.19/Workspace-2.19/EverisGit1InicialesRafaelLiebanaFuentes/src/HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo {
 
 	public static void main(final String[] args) {
 
-		String s = "Hola Mundo";
+		String s = "Hola Mundo, soy Rafael Liébana Fuentes y este es el segundo cambio";
 		System.out.println(s);
 
 	}
